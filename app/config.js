8cac38ffcd8f930843cf754e785a930e944dc13f
Lorenzo De Nobili <elledienne@gmail.com>
2015-11-06 09:30:15 -0800

    small bugfix

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 if(process.env.NODE_ENV !== 'production'){
   mongoose.connect('mongodb://localhost:27017/test');
 } else {
-  mongoose.connect(MONGO_CREDENTIALS);
+  mongoose.connect(process.env.MONGO_CREDENTIALS);
 }
 
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
